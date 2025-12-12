9e119fa9629775c8e053e523ec8eac28b303b58f
Jacob Cortez <96961142+Jcortez706@users.noreply.github.com>
2025-12-11 22:53:02 -0500

    Destroy shreddable tagged game objects

--- Assets/Scripts/Shredder/ShredderScript.cs
+++ Assets/Scripts/Shredder/ShredderScript.cs
@@ -4,14 +4,35 @@ public class ShredderScript : MonoBehaviour
 {
     private GameObject shredder;
     [SerializeField] private LayerMask pickupLayerMask;
+    private bool isHolding = false;
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision);
         if(collision.gameObject.CompareTag("Shreddable"))
         {
-            Debug.Log("Detected");
-            Destroy(collision.gameObject);
+            isHolding = true;
+            
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Shreddable"))
+        {
+            isHolding = false;
+
+        }
+    }
+
+    public void destroyHeldObject(GameObject document)
+    {
+        if (document.CompareTag("Shreddable"))
+        {
+            Destroy(document);
+        }
+        else
+        {
+            Debug.Log("Object not shreddable " + document);
         }
     }
 }
